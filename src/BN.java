1bbac310371d6d642fe8bdf8f032dfc54133a410
BlackShadow77 <BlackShadow77@users.noreply.github.com>
2016-07-20 03:30:28 +0200

    optimizations and terminated BN constructor with string

--- src/BN.java
+++ src/BN.java
@@ -8,13 +8,14 @@ public class BN {
 	private char s;
 	private String i,d;
 	private byte[] I,D;
-	static final char comma=',' , period='.';
-	static final char p='+', m='-';
+	private static final char comma=',' , period='.';
+	private static final char p='+', m='-';
 
 
 //costruttori
-
-	BN()
+	
+	//constructor null
+	public BN()
 	{
 		I=new byte[]{0};
 		D=new byte[]{0};
@@ -22,10 +23,13 @@ public class BN {
 		original="0";
 	}
 
-	BN(String input)
+	//constructor with the string that contain the number
+	public BN(String input)
 	{
+		//set original string
 		setOriginal(input);
-
+		
+		//set s
 		int n=0;
 		if(original.charAt(0)==p || original.charAt(0)==m)
 		{
@@ -35,57 +39,127 @@ public class BN {
 		else
 			setS(p);
 
-		String s1="";
-		boolean i1=true;
-
+		//declaration of boolean variable to use it later in try block
+		boolean b1=true;
+		
+		//set of i and d
 		try
 		{
-			for (; n < original.length(); n++)
+			while (n<original.length())
 			{
-				char c = original.charAt(n);
+				char c = original.charAt(++n);
 				switch (c)
 				{
 					case '0':
-						s1 = "0";
+						if (b1)
+						{
+							i=i.concat("0");
+						}
+						else
+						{
+							d=d.concat("0");
+						};
 					case '1':
-						s1 = "1";
+						if (b1)
+						{
+							i=i.concat("1");
+						}
+						else
+						{
+							d=d.concat("1");
+						};
 					case '2':
-						s1 = "2";
+						if (b1)
+						{
+							i=i.concat("2");
+						}
+						else
+						{
+							d=d.concat("2");
+						};
 					case '3':
-						s1 = "3";
+						if (b1)
+						{
+							i=i.concat("3");
+						}
+						else
+						{
+							d=d.concat("3");
+						};
 					case '4':
-						s1 = "4";
+						if (b1)
+						{
+							i=i.concat("4");
+						}
+						else
+						{
+							d=d.concat("4");
+						};
 					case '5':
-						s1 = "5";
+						if (b1)
+						{
+							i=i.concat("5");
+						}
+						else
+						{
+							d=d.concat("5");
+						};
 					case '6':
-						s1 = "6";
+						if (b1)
+						{
+							i=i.concat("6");
+						}
+						else
+						{
+							d=d.concat("6");
+						};
 					case '7':
-						s1 = "7";
+						if (b1)
+						{
+							i=i.concat("7");
+						}
+						else
+						{
+							d=d.concat("7");
+						};
 					case '8':
-						s1 = "8";
+						if (b1)
+						{
+							i=i.concat("8");
+						}
+						else
+						{
+							d=d.concat("8");
+						};
 					case '9':
-						s1 = "9";
+						if (b1)
+						{
+							i=i.concat("9");
+						}
+						else
+						{
+							d=d.concat("9");
+						};
 					case comma:
-						i1 = false;
+						b1 = false;
 					case period:
-						i1 = false;
+						b1 = false;
 					default:
 						throw new BNCharacterException();
 				}
-				if (i1)
-				{
-					i+=s1;
-				}
-				else
-				{
-					d+=s1;
-				}
 			}
 		}
+		
+		//exception if original contains character not valid
 		catch (BNCharacterException e)
 		{
 			System.out.println(e.getError());
 		}
+
+		//set I and D if d is not null
+		setI(stringToByte(geti()));
+		if(!d.equals(null))
+			setD(stringToByte(getd()));
 	}
 
 
@@ -118,7 +192,7 @@ public class BN {
 	{
 		return I;
 	}
-	public void setI (byte[] n)
+	private void setI (byte[] n)
 	{
 		I = n;
 	}
@@ -126,7 +200,7 @@ public class BN {
 	{
 		return D;
 	}
-	public void setD (byte[] d)
+	private void setD (byte[] d)
 	{
 		D = d;
 	}
@@ -134,7 +208,7 @@ public class BN {
 	{
 		return s;
 	}
-	public void setS (char s)
+	private void setS (char s)
 	{
 		this.s = s;
 	}
@@ -165,6 +239,17 @@ public class BN {
 		byte n;
 		n=Byte.parseByte(s1);
 		return n;
+	}
+
+	//convert a string in a byte array
+	private byte[] stringToByte(String s1)
+	{
+		byte[] b = new byte[s1.length()];
+		for (int i = 0; i < s1.length(); i++)
+		{
+			b[i] = charToByte(s1.substring(i, i + 1));
+		}
+		return b;
 	}
 
 }
