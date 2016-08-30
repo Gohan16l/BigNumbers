7573dde2db26b6a96020dbda24af64e6fe9363e7
BlackShadow77 <BlackShadow77@users.noreply.github.com>
2016-08-30 04:46:43 +0200

    Little bug fix

--- src/com/core/Main.java
+++ src/com/core/Main.java
@@ -1,7 +1,7 @@
 package com.core;
 
 import BigNumbers.BN;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	private static Scanner keyboard = new Scanner(System.in);
@@ -15,7 +15,7 @@ public class Main {
 	public static void main(String[] args) {
 	// write your code here
 
-		println("Run TEST Version 0.0.3.2 - BigNumbers library test");//-- KILLUA APPROVED
+		println("Run TEST Version 0.0.3.2.1 - BigNumbers library test");//-- KILLUA APPROVED
 
 		println("First number:");
 		BN a = new BN(keyboard.nextLine());
@@ -28,6 +28,7 @@ public class Main {
 		//System.out.println(a.getOriginal());
 
 		println("select operation (+ - x ÷) :");
+		long startTime = System.currentTimeMillis();
 		switch (keyboard.next())
 		{
 			case "+" :c = a.sum(b); break;
@@ -35,8 +36,7 @@ public class Main {
 			default: println("Character not valid or work in progress"); c = new BN(); break;
 		}
 
-
-
+		long finalTime = System.currentTimeMillis();
 
 		BN d = new BN("0");
 
@@ -45,5 +45,16 @@ public class Main {
 		System.out.println(BN.abs(c));
 		println(String.valueOf("digits used:"+c.length()));
 		println(String.valueOf("ID: "+d.getID()));
-    }
+		if (((finalTime - startTime) / 1000) >= 60)
+		{
+			long minutes = ((finalTime - startTime) / 1000) / 60;
+			long ms = minutes * 60;
+			long seconds = ((finalTime - startTime) / 1000) - ms;
+			System.out.println("execution time: " + minutes + " minutes and " + seconds + " seconds");
+		}
+		else
+		{
+			System.out.println("execution time: " + ((finalTime - startTime) / 1000) + " seconds");
+		}
+	}
 }
